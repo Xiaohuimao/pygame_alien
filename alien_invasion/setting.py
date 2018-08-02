47ef2a74053299a7f3c96fa44079bb3230da0577
Xihuimao <m0926xf@126.com>
2018-08-02 14:54:39 +0800

    alien_invasion

---
+++ alien_invasion/setting.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class Setting():
+	#存储《外星人入侵》的所有设置的类
+	def __init__(self):
+		#初始化游戏的设置
+		#屏幕设置
+		self.screen_width=1200
+		self.screen_height=600
+		self.bg_color=(230,230,230)
+		#飞船的设置(centerx只能储存整数值)
+		self.ship_speed_factor=1.5
+		#子弹设置
+		self.bullet_speed_factor=1#速度
+		self.bullet_width=3#宽
+		self.bullet_height=15#高
+		self.bullet_color=60,60,60#颜色
+		self.bullets_allowed=3#允许同时出现在屏幕的最大子弹数
+		
+	
